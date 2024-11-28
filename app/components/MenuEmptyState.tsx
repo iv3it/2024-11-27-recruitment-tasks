96d9d8e4e6aaef60fdc7ce1483805ffe15f54a51
Kinga <kinga.sabat@gmail.com>
2024-11-28 02:46:13 +0100

    box-shadow fix

--- app/components/MenuEmptyState.tsx
+++ app/components/MenuEmptyState.tsx
@@ -7,7 +7,7 @@ export default function MenuEmptyState() {
         <h1 className='font-semibold text-base text-center text-[var(--blueDark)]'>Menu jest puste</h1>
         <h2 className='text-sm text-center text-[var(--greyMedium)]'>W tym menu nie ma jeszcze żadnych linków.</h2>
       </div>
-      <button className='shadow-sm shadow-[rgba(16, 24, 40, 0.05)] self-center py-[10px] px-4 flex bg-[var(--purpleMedium)] border border-[var(--purpleMedium)] rounded-lg'>
+      <button className='shadow-[0_1px_2px_0px_rgba(16,24,40,0.05)] self-center py-[10px] px-4 flex bg-[var(--purpleMedium)] border border-[var(--purpleMedium)] rounded-lg'>
         <PlusCircleIcon className="size-5 text-white mr-1" />
         <span className='text-sm text-white font-semibold'>Dodaj pozycję menu</span>
       </button>
